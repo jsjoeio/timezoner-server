ab6e70dc9aaec87e599198050901e465fdabb8a2
Joe Previte <jjprevite@gmail.com>
2018-12-18 14:20:39 -0700

    Refactor to serverless function

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const axios = require('axios')
-const cors = require('cors')
-const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(cors())
-
-app.get('/', (req, res) => res.send("Hey! You're not supposed to be here! 😱"))
-
-app.post('/api/bitly/', (req, res) => {
+module.exports = (req, res) => {
   const longUrl = req.body.long_url
   const options = {
     headers: {
@@ -38,6 +26,4 @@ app.post('/api/bitly/', (req, res) => {
     .catch(function(error) {
       console.log(error)
     })
-})
-
-app.listen(3000, () => console.log('timezoner-server listening on port 3000!'))
+};
